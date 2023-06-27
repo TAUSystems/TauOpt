1b0ccf907b180356a89b0f46c3f902398feb44d1
rahul-tau <137096689+rahul-tau@users.noreply.github.com>
2023-06-27 11:52:41 -0400

    Add files via upload

--- src/io/parse_input.py
+++ src/io/parse_input.py
@@ -2,7 +2,20 @@
 
 import os
 
-def user_config_file_name(fname):
+def user_config_file_name(str_in):
     """ extract python file name (config file) from the input string  """
-    fname_parsed = fname.split(".")[0].lstrip('-').strip()
-    return fname_parsed
+    
+    str_in = str_in.split(".")[0].lstrip('-').strip()
+
+    _, fname_parsed = os.path.split(str_in)
+    
+    fname_parsed = fname_parsed+'.py'
+    path_parsed = str_in+'.py'
+
+    if not os.path.isfile(path_parsed):
+        print(f" The configuration file could not be found ! ")
+        print(f" Please prodive a correct full path (with the script name) as an argument to TauOpt.py. ")
+        print(f" Example: python3 TauOpt.py /Users/Project/my_config.py \n ")
+        print(f" The path you provided : {path_parsed} ")
+    
+    return fname_parsed, path_parsed
