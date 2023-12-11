1b523da2591034e4e3f2192cba35b34efa7ebbe7
Miguel Gracia <miguel.gracia@tausystems.com>
2023-12-11 14:33:04 -0600

    Example typos fixed

--- examples/test_project/test_config.py
+++ examples/test_project/test_config.py
@@ -9,7 +9,7 @@ project_folder='/FULL PATH /TauOpt/examples/test_project'
 
 code_name = 'test_code'
 exec_name = 'test_code'
-input_file = ["input.txt"]
+input_files = ["input.txt"]
 
 #-----------------------------------------------------------------------------------------------
 # parameters that change from run to run
@@ -20,7 +20,7 @@ scan_type = 'opt' # func : predefined function; opt : optimization; file: read f
 var_names = ['x1','x2']#['x1','x2','x3']
 var_val0 = [3, 8] # initial value of the varaibles
 var_range = {'x1':[1, 10], 'x2':[1, 10]}
-tolerance = {'x1': 0.1, 'x2': 0.1}
+var_tolerance = {'x1': 0.1, 'x2': 0.1}
 
 #reading values of 'var_names' from a file
 #var_filename = 'test_param.txt'
